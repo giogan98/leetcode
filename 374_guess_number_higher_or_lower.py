be37aa512ffef39b4a09184d2db2f6c2fd41eb04
Giorgio Gandolfi <giorgio.gandolfi98@gmail.com>
2022-11-16 16:54:46 +0100

    Solved problem n 374

---
+++ 374_guess_number_higher_or_lower.py
@@ -0,0 +1,43 @@
+'''
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is
+higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible
+results:
+
+    -1: Your guess is higher than the number I picked (i.e. num > pick).
+    1: Your guess is lower than the number I picked (i.e. num < pick).
+    0: your guess is equal to the number I picked (i.e. num == pick).
+
+Return the number that I picked.
+'''
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    # Implemented with a basic binary search
+    def guessNumber(self, n: int) -> int:
+         
+        min = 1
+        max = n
+        
+        while True:
+            
+            prov = (min + max) // 2
+
+            # Go higher
+            if guess(prov) == 1:
+                min = prov + 1
+            # Go lower
+            elif guess(prov) == -1:
+                max = prov - 1
+            else:
+                return prov
